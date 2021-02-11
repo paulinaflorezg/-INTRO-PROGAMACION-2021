71d907bb88c6b356621ace9fa93591d146860215
paulinaflorezg <florezg.maria@UCES.EDU.CO>
2021-02-11 07:42:02 -0500

    bien

---
+++ clases/operaciones.py
@@ -0,0 +1,13 @@
+numeroA = 86
+numeroB = 84
+sumar = numeroA + numeroB
+print("el resultado de la suma es" , sumar)
+print (f"la suma dio ¨{sumar} exitosamente")
+restar = numeroA - numeroB
+print(f"la resta dio {restar} exitosamente")
+multiplicar = numeroA * numeroB
+print (f"la multiplicacion dio {multiplicar}exitosamente")
+dividir = numeroA / numeroB
+print (f"la dividicion dio {dividir} exitosamente")
+exponente = numeroA**numeroB
+print (f"la operacion exponencial dio {exponente}exitosamente")
