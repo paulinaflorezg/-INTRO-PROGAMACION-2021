87ef3ebce26f2ff67d2dc944379f3b9cb82f335d
paulinaflorezg <florezg.maria@UCES.EDU.CO>
2021-02-15 17:38:41 -0500

    termine

---
+++ talleres/taller1.py
@@ -0,0 +1,35 @@
+#Taller de practica de operaciones y mas
+pruebaF = "falso"
+pruebaV = "verdadero"
+print (pruebaF)
+print (pruebaV)
+#datos
+numeroA = 30
+numeroB = 24
+#Preguntar cual es mayor y cual es menor
+isNumeroMayor = numeroA > numeroB
+print ("#"*10, "A es mayor que B", "#"*10)
+print (isNumeroMayor)
+isNumeroMenor = numeroA  < numeroB
+print ("#"*10, "A es menor que B", "#"*10)
+print (isNumeroMenor)
+#Preguntar si son iguales
+isIgual = numeroA == numeroB
+print ("#"*10, "A es igual que B","#"*10)
+print (isIgual)
+#Operaciones 
+#suma
+sumar = numeroA + numeroB
+print (f"la suma dio {sumar} exitosamente")
+#resta
+resta = numeroA - numeroB
+print (f"la resta dio {resta} exitosamente")
+#multiplicacion
+multiplicar = numeroA * numeroB
+print (f"la multiplicacion dio {multiplicar} exitosamente")
+#division
+dividir = numeroA / numeroB
+print (f"la division dio {dividir} exitosamente")
+#exponente
+exponente = numeroA ** numeroB
+print (f"la operacion exponencial dio {exponente} exitosamente ")
