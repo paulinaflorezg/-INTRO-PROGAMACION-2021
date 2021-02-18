d53f9e1b9eb3bd824faef7a87aa61135553aded4
paulinaflorezg <florezg.maria@UCES.EDU.CO>
2021-02-17 20:58:18 -0500

    TALLER EXCELENTE

--- talleres/taller2.py
+++ talleres/taller2.py
@@ -1,35 +1,34 @@
-#Taller de practica de operaciones y mas
-pruebaF = "falso"
-pruebaV = "verdadero"
-print (pruebaF)
-print (pruebaV)
-#datos
-numeroA = 30
-numeroB = 24
-#Preguntar cual es mayor y cual es menor
-isNumeroMayor = numeroA > numeroB
-print ("#"*10, "A es mayor que B", "#"*10)
+MENSAJE_BIENVENIDA = "Hola como estas? me puedes decir dos numeros"
+MENSAJE1 = "Tu numero uno es : "
+MENSAJE2 = "Tu numero dos es : "
+
+
+print (MENSAJE_BIENVENIDA)
+NUMERO1= int(input(MENSAJE1))
+NUMERO2= int(input(MENSAJE2))
+
+isNumeroMayor = NUMERO1 > NUMERO2
+print ("#"*10, "NUMERO1 es mayor que NUMERO2", "#"*10)
 print (isNumeroMayor)
-isNumeroMenor = numeroA  < numeroB
-print ("#"*10, "A es menor que B", "#"*10)
+isNumeroMenor = NUMERO1  < NUMERO2
+print ("#"*10, "NUMERO1 es menor que NUMERO2", "#"*10)
 print (isNumeroMenor)
-#Preguntar si son iguales
-isIgual = numeroA == numeroB
-print ("#"*10, "A es igual que B","#"*10)
+
+isIgual = NUMERO2 == NUMERO2
+print ("#"*10, "NUMERO1 es igual que NUMERO21","#"*10)
 print (isIgual)
-#Operaciones 
-#suma
-sumar = numeroA + numeroB
+
+sumar = NUMERO1 + NUMERO2
 print (f"la suma dio {sumar} exitosamente")
 #resta
-resta = numeroA - numeroB
+resta = NUMERO1 - NUMERO2
 print (f"la resta dio {resta} exitosamente")
 #multiplicacion
-multiplicar = numeroA * numeroB
+multiplicar = NUMERO1 * NUMERO2
 print (f"la multiplicacion dio {multiplicar} exitosamente")
 #division
-dividir = numeroA / numeroB
+dividir = NUMERO1 / NUMERO2
 print (f"la division dio {dividir} exitosamente")
 #exponente
-exponente = numeroA ** numeroB
+exponente = NUMERO1 ** NUMERO2
 print (f"la operacion exponencial dio {exponente} exitosamente ")
