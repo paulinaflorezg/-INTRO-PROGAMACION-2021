0c8c2117bd6280a2299f0c453f76bb880ead8468
paulinaflorezg <florezg.maria@UCES.EDU.CO>
2021-04-21 21:00:06 -0500

    BIEN

---
+++ talleres/tallerobjetos.py
@@ -0,0 +1,13 @@
+#CLASETORTA
+class torta ():
+    def_init_(self,sabornentrada,formaentrada,alturaentrada):
+        self.sabor = sabornentrada
+        self.forma = formaentrada
+        self.altura = alturaentrada
+def atributos (self):
+    print(f'hola mi sabor es {self.sabor} y mi forma es{self.forma}y mi altura es {self.altura})
+
+torta1 = torta ('vainilla','redonda',15)
+torta1.atributos ()
+
+#CLASE 
