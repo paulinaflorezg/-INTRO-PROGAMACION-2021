d04af8107e74dd4348afa00a703ca37a4394393f
paulinaflorezg <florezg.maria@UCES.EDU.CO>
2021-04-21 22:20:29 -0500

    triste

--- talleres/tallerobjetos.py
+++ talleres/tallerobjetos.py
@@ -1,13 +1,23 @@
 #CLASETORTA
 class torta ():
-    def_init_(self,sabornentrada,formaentrada,alturaentrada):
-        self.sabor = sabornentrada
+    def_init_(self,saborentrada,formaentrada,alturaentrada)
+
+        self.sabor = saborentrada
         self.forma = formaentrada
         self.altura = alturaentrada
-def atributos (self):
-    print(f'hola mi sabor es {self.sabor} y mi forma es{self.forma}y mi altura es {self.altura})
+    def atributos (self):
+        print(f'hola mi sabor es {self.sabor} y mi forma es{self.forma}y mi altura es {self.altura})
 
 torta1 = torta ('vainilla','redonda',15)
 torta1.atributos ()
 
-#CLASE 
+#CLASE ESTUDIANTE
+class estudiante ():
+    def_init_(self,nombrentanda,edadentrada,carreraentrada,identrada,semestrentrada):
+        self.nombre = nombrentanda
+        self.edad = edadentrada
+        self.carrera = carreraentrada
+        self.id = identrada
+        self.semestre = semestrentrada
+
+
